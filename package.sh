77335ca3121cbc7614237e7d7b732feba1bd60da
sogaani <soga.ani@gmail.com>
2018-02-17 01:11:41 +0900

    support rm3 pro plus

--- package.sh
+++ package.sh
@@ -7,7 +7,7 @@ sha256sum *.js LICENSE > SHA256SUMS
 find node_modules/ -type f -exec sha256sum {} \; >> SHA256SUMS
 TARFILE=$(npm pack)
 tar xf ${TARFILE}
-cp -r node_modules ./package
+cp -r devices node_modules ./package
 tar cf ${TARFILE} package
 rm -rf package
 echo "Created ${TARFILE}"
