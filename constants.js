af33d5a95724092492f2a3bd62ada515114a9dad
sogaani <soga.ani@gmail.com>
2018-03-03 02:27:48 +0900

    remove unused package

--- constants.js
+++ constants.js
@@ -1,3 +1,5 @@
+'use strict';
+
 exports.IR_DEVICE_TYPE_THERMOSTAT = 'thermostat';
 exports.IR_DEVICE_TYPE_ON_OFF_LIGHT = 'light';
 exports.IR_DEVICE_TYPE_DIMMABLE_LIGHT = 'dimmableLight';
--- package.sh
+++ package.sh
@@ -4,10 +4,10 @@ rm -rf node_modules
 npm install
 rm -f SHA256SUMS
 sha256sum *.js LICENSE > SHA256SUMS
-#find node_modules/ -type f -exec sha256sum {} \; >> SHA256SUMS
+find node_modules/ -type f -exec sha256sum {} \; >> SHA256SUMS
 TARFILE=$(npm pack)
 tar xzf ${TARFILE}
-cp -r devices node_modules ./package
+cp -r asset node_modules ./package
 tar czf ${TARFILE} package
 rm -rf package
 echo "Created ${TARFILE}"
